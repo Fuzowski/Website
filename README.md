23fe971e5ca42fdfe587300e0503d757a0b799a4
unknown <Everett.Horton95@yahoo.com>
2025-01-09 17:42:51 -0500

    in process

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Website
-This repository is for my website
+
+This repository is for my website. It will be used as something for me to go back to and add onto the further i get into class.
---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("myH1").textContent = 'Pro Gamer, Coach, Future Developer' ;
+
+// Write the content for you about you paragraogh here
+document.getElementById("myP").textContent = 'I have the dream to one day be able to travel the world while being a software engineer!' ;
