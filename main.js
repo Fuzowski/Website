407a98d3ede06db60786ff84d305cd59af048919
unknown <Everett.Horton95@yahoo.com>
2025-02-20 19:31:58 -0500

    my Website

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-document.getElementById("myH1").textContent = 'Pro Gamer, Coach, Future Developer' ;
+document.getElementById("myH1").textContent =
+  "Pro Gamer, Coach, Future Developer";
 
 // Write the content for you about you paragraogh here
-document.getElementById("myP").textContent = 'I have the dream to one day be able to travel the world while being a software engineer!' ;
+document.getElementById("myP").textContent =
+  "I have the dream to one day be able to travel the world while being a software engineer!";
+
+  let age = 29
+  let fullname = 'Everett Horton'
+ 
+console.log(typeof age)
+console.log(`You are ${age} years old`)
+console.log(`Your Name is ${fullname}`)
+
+document.getElementById("myH3").textContent = "My Goals";
+
